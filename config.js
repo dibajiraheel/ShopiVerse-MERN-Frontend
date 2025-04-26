76b7976a857d2be74960f3248c7d03d24df57d29
unknown <dibajiraheel@gmail.com>
2025-04-27 01:04:54 +0500

    spellingmistake corrected

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const config = {
     'apiBaseUrl': String(import.meta.env.VITE_API_BASE_URL),
     'sellergoogleloginapiurl': String(import.meta.env.VITE_SELLER_GOOGLE_LOGIN_API_URL),
-    'customergoogleloginapiurl': String(import.meta.env.VIRE_CUSTOMER_GOOGLE_LOGIN_API_URL)
+    'customergoogleloginapiurl': String(import.meta.env.VITE_CUSTOMER_GOOGLE_LOGIN_API_URL)
 }
 
 
